946bdea82c8a4388497d068a58055e463143bf31
ThibautDalez <thibaut.dalez@student.kuleuven.be>
2023-05-15 21:54:39 +0200

    prutsen

--- Motordinges/src/main.cpp
+++ Motordinges/src/main.cpp
@@ -1,73 +1,16 @@
 // code voor de esp's aan de motoren
-// naam esp: espmotor
-// wachtwoord esp: espmotor
 
 #include <Arduino.h>
 #include <esp_now.h>
 #include <WiFi.h>
+
 #include "OTAlib.h"
 #include <PubSubClient.h>
 #include <esp_wifi.h>
 
 //OTA
-OTAlib ota("NETGEAR68", "excitedtuba713");
+OTAlib ota("WiFi-2.4-017A", "5FAFC34859");
 
-//MQTT -
-#define SSID1          "NETGEAR68"
-#define PWD           "excitedtuba713"
-#define MQTT_SERVER   "192.168.1.61"  
-#define MQTT_PORT     1883
-#define topic  "esp_motor/output"
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-void setup_wifi()
-{
-  delay(10);
-  Serial.println("Connecting to WiFi..");
-  WiFi.begin(SSID1, PWD);
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.print("MAC address: ");
-  Serial.println(WiFi.macAddress());
-}
-
-void reconnect()
-{
-  // Loop until we're reconnected
-  while (!client.connected())
-  {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    // creat unique client ID
-    // in Mosquitto broker enable anom. access
-    if (client.connect("ESP32Client"))
-    {
-      Serial.println("connected");
-      // Subscribe
-      client.subscribe(topic);
-    }
-    else
-    {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 1 second");
-      vTaskDelay(1000/portTICK_RATE_MS);
-    }
-    taskYIELD();
-  }
-}
-//- MQTT
 
 #define JOYSTICK_LAAG 500
 #define JOYSTICK_HOOG 1500
@@ -102,28 +45,10 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   Serial.println(incoming_vrx);
 }
 
-int32_t getWiFiChannel(const char *ssid) {
-  if (int32_t n = WiFi.scanNetworks()) {
-      for (uint8_t i=0; i<n; i++) {
-          if (!strcmp(ssid, WiFi.SSID(i).c_str())) {
-              return WiFi.channel(i);
-          }
-      }
-  }
-  return 0;
-}
-
 void setup() {
-
-
   Serial.begin(115200);
 
-  //MQTT -
-  setup_wifi();
-  // client.setServer(MQTT_SERVER, MQTT_PORT);
-  // - MQTT
-
-  // OTA
+  //OTA
   ota.setHostname("espmotor1");  
   ota.setPassword("espmotor1");
   ota.begin();
@@ -133,39 +58,25 @@ void setup() {
   pinMode(pin_dir1, OUTPUT);
   pinMode(pin_pwm2, OUTPUT);
   pinMode(pin_dir2, OUTPUT);
-  
-  digitalWrite(pin_pwm1, HIGH);  // motor 1 draait niet
-  digitalWrite(pin_pwm2, HIGH);  // motor 2 draait niet
-  Serial.println("motoren draaien niet");
+
+
   //communicatie
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_AP_STA);
-  int32_t channel =getWiFiChannel(SSID1);
-  esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
 
   // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
+    return;
   }
 
   // Register for a callback function that will be called when data is received
   esp_now_register_recv_cb(OnDataRecv);
 
+
 }
 
 void loop() {
-
-  //MQTT -
-  // if (!client.connected()){
-  //   reconnect();
-  // }
-  // client.loop();
-  //- MQTT
-  //client.publish(topic, "loop");
-  client.publish(topic, (char*) incoming_vrx);
-  client.publish(topic, (char*) incoming_vry);
-  // client.publish(topic, (char*) WiFi.macAddress);
-
   // motor 1 besturen met vrx
   if(incoming_vrx < JOYSTICK_LAAG){
     digitalWrite(pin_dir1, HIGH);  // motor 1 draait in ene richting
@@ -183,6 +94,7 @@ void loop() {
   if(incoming_vry < JOYSTICK_LAAG){
     digitalWrite(pin_dir2, HIGH);  // motor 2 draait in ene richting
     digitalWrite(pin_pwm2, LOW);  // motor 2 draait
+    
   }
   else if(incoming_vry > JOYSTICK_HOOG){
     digitalWrite(pin_dir2, LOW);  // motor 2 draait in andere richting
@@ -193,6 +105,5 @@ void loop() {
   }
 
   delay(200);  // elke 0.2s opnieuw meten en doorsturen
-  //taskYIELD();
-
+  taskYIELD();
 }
